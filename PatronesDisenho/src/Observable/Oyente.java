1f256e7eda9261637c6c4adbc0ff22713be68662
tecnico <tecnico@192.168.1.159>
2016-06-25 01:37:45 +0200

    Paso 3.- El emisor tiene que tener métodos para añadir y eliminar un     oyente al que avisar.

--- PatronesDisenho/src/Observable/Oyente.java
+++ PatronesDisenho/src/Observable/Oyente.java
@@ -7,6 +7,8 @@ package Observable;
     1.- Nos obliga a crear una interfaz con métodos. La cantidad de 
     métodos depende de cuantas cosas queráis avisar. 
     2.- Los oyentes tienen que implementar la interfaz.
+    3.- El emisor tiene que tener métodos para añadir y eliminar un 
+    oyente al que avisar. 
 */
 public class Oyente implements OyenteListener{
 
