f5d2b518a5157837b3f255072978b6ddacabfca3
Lupercar <hotolete@gmail.com>
2016-06-25 01:09:04 +0200

    Update Oyente.java
    Cambio algo en clase Oyente

--- PatronesDisenho/src/Observable/Oyente.java
+++ PatronesDisenho/src/Observable/Oyente.java
@@ -6,7 +6,7 @@ package Observable;
 
     1.- Nos obliga a crear una interfaz con métodos. La cantidad de 
     métodos depende de cuantas cosas queráis avisar. 
-    
+    2.- Los oyentes tienen que implementar la interfaz.
 */
 public class Oyente {
     
