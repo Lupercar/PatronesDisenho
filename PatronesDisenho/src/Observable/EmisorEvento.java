94f7b7d1b0c021a80410c913e74302586043bb2e
tecnico <tecnico@192.168.1.159>
2016-06-25 00:51:11 +0200

    Creacion clase EmisorEvento

---
+++ PatronesDisenho/src/Observable/EmisorEvento.java
@@ -0,0 +1,13 @@
+package Observable;
+
+public class EmisorEvento {
+   private int valor;
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+}//fin class EmisorEvento
