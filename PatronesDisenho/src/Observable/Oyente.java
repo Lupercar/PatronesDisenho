32214ed257c390f2a2f5761b4f664b19fcd7fe56
tecnico <tecnico@192.168.1.159>
2016-06-25 01:04:33 +0200

    Creacion clase Oyente

---
+++ PatronesDisenho/src/Observable/Oyente.java
@@ -0,0 +1,13 @@
+package Observable;
+
+/*
+     El patron observable se usa para avisar a otros objetos
+    (instancias en memoria) que un objeto ha cambiado(lanza un evento).
+
+    1.- Nos obliga a crear una interfaz con métodos. La cantidad de 
+    métodos depende de cuantas cosas queráis avisar. 
+    
+*/
+public class Oyente {
+    
+}//fin class Oyente
