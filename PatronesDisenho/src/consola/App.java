f551a67b07d3df655fa4330be8b997f314fd71b6
tecnico <tecnico@192.168.1.159>
2016-06-25 02:45:30 +0200

    Hago prueba del patron Observer

--- PatronesDisenho/src/consola/App.java
+++ PatronesDisenho/src/consola/App.java
@@ -1,8 +1,16 @@
 package consola;
 
+import Observable.EmisorEvento;
+import Observable.Oyente;
+
 public class App {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        EmisorEvento e = new EmisorEvento();
+        Oyente o = new Oyente();
+        
+        e.addEmisorOyenteListener(o);
+        e.setValor(100);
     }
 }//fin class App
